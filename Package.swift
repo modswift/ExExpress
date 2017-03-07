f8fd8ca7e79eaa2e6c865413302baf7a82e8700f
Helge Hess <devteam@zeezide.de>
2017-03-07 22:48:16 +0100

    Refer to own Freddy clone

--- Package.swift
+++ Package.swift
@@ -8,8 +8,14 @@ let package = Package(
 		         majorVersion: 1, minor: 0),
 	  .Package(url: "https://github.com/AlwaysRightInstitute/mustache.git", 
 		         majorVersion: 0, minor: 5),
+    
+#if true             
+ 	  .Package(url: "https://github.com/modswift/Freddy.git", 
+ 		         majorVersion: 3, minor: 0)
+#else fails on Linux
 	  .Package(url: "https://github.com/bignerdranch/Freddy.git",
 						 majorVersion: 3, minor: 0),
+#endif
   ],
 	
 	exclude: [
