900a8cf4073ca5e9c269a0fbf52e11f87bf2a145
Helge Hess <devteam@zeezide.de>
2017-03-31 19:05:57 +0200

    Move RoutePattern to own file, prep prefix match
    ... we need to do a prefix match.
    Also prep support for baseURL.
    .. and $ pattern.

--- Sources/ExExpress/express/Route.swift
+++ Sources/ExExpress/express/Route.swift
@@ -7,7 +7,6 @@
 //
 
 private let patternMarker : UInt8 = 58 // ':'
-private let debugMatcher  = false
 
 /**
  * A Route is a middleware which wraps another middleware and guards it by a
@@ -153,9 +152,13 @@ open class Route: MiddlewareObject, RouteKeeper, CustomStringConvertible {
       // TODO: consider mounting!
       let escapedPathComponents = split(urlPath: req.url)
       
-      if !RoutePattern.match(pattern: pattern, against: escapedPathComponents) {
+      guard let match = RoutePattern.match(pattern: pattern,
+                                           against: escapedPathComponents)
+       else {
         return false
-      }
+       }
+      
+      if debug { console.log("\(#function) match:", match) }
     }
     
     return true
@@ -236,159 +239,6 @@ open class Route: MiddlewareObject, RouteKeeper, CustomStringConvertible {
     return ms
   }
   
-}
-
-enum RoutePattern : CustomStringConvertible {
-  case Root
-  case Text    (String)
-  case Variable(String)
-  case Wildcard
-  case Prefix  (String)
-  case Suffix  (String)
-  case Contains(String)
-  
-  func match(string s: String) -> Bool {
-    switch self {
-      case .Root:            return s == ""
-      case .Text(let v):     return s == v
-      case .Wildcard:        return true
-      case .Variable:        return true // allow anything, like .Wildcard
-      case .Prefix(let v):   return s.hasPrefix(v)
-      case .Suffix(let v):   return s.hasSuffix(v)
-      case .Contains(let v): return s.contains(v)
-    }
-  }
-  
-  public var description : String {
-    switch self {
-      case .Root:             return "/"
-      case .Text(let v):      return v
-      case .Wildcard:         return "*"
-      case .Variable (let n): return ":\(n)"
-      case .Prefix(let v):    return "\(v)*"
-      case .Suffix(let v):    return "*\(v)"
-      case .Contains(let v):  return "*\(v)*"
-    }
-  }
-
-  /**
-   * Creates a pattern for a given 'url' string.
-   *
-   * - the "*" string is considered a match-all.
-   * - otherwise the string is split into path components (on '/')
-   * - if it starts with a "/", the pattern will start with a Root symbol
-   * - "*" (like in `/users/ * / view`) matches any component (spaces added)
-   * - if the component starts with `:`, it is considered a variable.
-   *   Example: `/users/:id/view`
-   * - "text*", "*text*", "*text" creates hasPrefix/hasSuffix/contains patterns
-   * - otherwise the text is matched AS IS
-   */
-  static func parse(_ s: String) -> [ RoutePattern ]? {
-    if s == "*" { return nil } // match-all
-    
-    var url = URL()
-    url.path = s
-    let comps = url.escapedPathComponents!
-    
-    var isFirst = false
-    
-    var pattern : [ RoutePattern ] = []
-    for c in comps {
-      if isFirst {
-        isFirst = false
-        if c == "" { // root
-          pattern.append(.Root)
-          continue
-        }
-      }
-      
-      if c == "*" {
-        pattern.append(.Wildcard)
-        continue
-      }
-      
-      if c.hasPrefix(":") {
-        let vIdx = c.index(after: c.startIndex)
-        pattern.append(.Variable(c[vIdx..<c.endIndex]))
-        continue
-      }
-      
-      if c.hasPrefix("*") {
-        let vIdx = c.index(after: c.startIndex)
-        if c == "**" {
-          pattern.append(.Wildcard)
-        }
-        else if c.hasSuffix("*") && c.characters.count > 1 {
-          let eIdx = c.index(before: c.endIndex)
-          pattern.append(.Contains(c[vIdx..<eIdx]))
-        }
-        else {
-          pattern.append(.Prefix(c[vIdx..<c.endIndex]))
-        }
-        continue
-      }
-      if c.hasSuffix("*") {
-        let eIdx = c.index(before: c.endIndex)
-        pattern.append(.Suffix(c[c.startIndex..<eIdx]))
-        continue
-      }
-
-      pattern.append(.Text(c))
-    }
-    
-    return pattern
-  }
-  
-  
-  // MARK: - Pattern Matching
-  
-  static func match(pattern p: [ RoutePattern ],
-                    against escapedPathComponents: [ String ]) -> Bool
-  {
-    var pattern = p
-    
-    if debugMatcher {
-      print("MATCH: components: \(escapedPathComponents)\n" +
-            "  against: \(pattern)")
-    }
-    
-    // this is to support matching "/" against the "/*" ("", "*") pattern
-    if escapedPathComponents.count + 1 == pattern.count {
-      if case .Wildcard = pattern.last! {
-        let endIdx = pattern.count - 1
-        pattern = Array<RoutePattern>(pattern[0..<endIdx])
-      }
-    }
-    
-    guard escapedPathComponents.count >= pattern.count else { return false }
-    
-    var lastWasWildcard = false
-    for i in pattern.indices {
-      let patternComponent = pattern[i]
-      let matchComponent   = escapedPathComponents[i]
-      
-      guard patternComponent.match(string: matchComponent) else {
-        return false
-      }
-      
-      if debugMatcher {
-        print("  MATCHED[\(i)]: \(patternComponent) \(matchComponent)")
-      }
-      
-      // Special case, last component is a wildcard. Like /* or /todos/*. In
-      // this case we ignore extra URL path stuff.
-      if case .Wildcard = patternComponent {
-        let isLast = i + 1 == pattern.count
-        if isLast { lastWasWildcard = true }
-      }
-    }
-    
-    if escapedPathComponents.count > pattern.count {
-      if !lastWasWildcard { return false }
-    }
-    
-    return true
-  }
 }
 
 
