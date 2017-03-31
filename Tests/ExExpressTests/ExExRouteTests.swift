0710d5615b2f5ee1bf7e9a785856c2ab41d4e463
Helge Hess <devteam@zeezide.de>
2017-03-31 18:15:20 +0200

    Setup unit tests bundle

---
+++ Tests/ExExpressTests/ExExRouteTests.swift
@@ -0,0 +1,20 @@
+//
+//  ExpressTests.swift
+//  ExpressTests
+//
+//  Created by Helge Hess on 31/03/17.
+//  Copyright © 2017 ZeeZide GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import ExExpress
+
+class ExExRouteTests: XCTestCase {
+    
+  
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
