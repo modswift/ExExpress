3dcf28608b3fc8a2e023e6d979b9f19424cccecc
Helge Hess <devteam@zeezide.de>
2017-05-02 01:36:54 +0200

    Mo QS preps

--- Sources/ExExpress/express/QS.swift
+++ Sources/ExExpress/express/QS.swift
@@ -12,11 +12,21 @@ public enum qs {
   // TODO: this is a little funky because URL parsing really happens at a byte
   //       level (% decoding etc)
   
+  struct Options {
+    let separator      : Character = "&"
+    let pairSeparator  : Character = "="
+    let depth          : Int       = 5
+    let parameterLimit : Int       = 1000
+    let arrayLimit     : Int       = 20
+    let allowsDot      : Bool      = false
+  }
+  
   public static func parse(_ string       : String,
                            separator      : Character = "&",
                            pairSeparator  : Character = "=",
                            depth          : Int       = 5,
                            parameterLimit : Int       = 1000,
+                           arrayLimit     : Int       = 20,
                            allowsDot      : Bool      = false)
                      -> Dictionary<String, Any>
   {
@@ -30,4 +40,13 @@ public enum qs {
     
     return qv
   }
+}
+
+// 'foo[bar][baz]=foobarbaz' -> [ "foo": [ "bar": [ "baz": "foobarbaz" ] ] ]
+// 'a[]=b&a[]=c')            -> [ "a": [ "b", "c" ] ]
+// 'a[1]=b&a[3]=c'           -> [ "a": [ nil, "b", nil, "c" ] ]  (max: 20)
+// 'a.b=c' (allowsDot)       -> [ "a": [ "b": "c" ] ]
+enum QueryParameterKeyPart {
+  case Key(String) // hello or [hello]
+  case Index(Int)  // [1]
 }
