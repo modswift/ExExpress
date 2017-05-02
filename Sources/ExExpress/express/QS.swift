7fdf0d082a900f30284de28a749df839106e2443
Helge Hess <devteam@zeezide.de>
2017-05-02 15:37:47 +0200

    Ugly but working `qs` module implementation
    The implementation of this is really awkward. Need to
    do better, not 100% yet how.

--- Sources/ExExpress/express/QS.swift
+++ Sources/ExExpress/express/QS.swift
@@ -20,6 +20,9 @@ public enum qs {
     let arrayLimit     : Int       = 20
     let allowsDot      : Bool      = false
   }
+
+  class EmptyArraySlot {}
+  public static let sparseArrayDefaultValue : Any = EmptyArraySlot()
   
   public static func parse(_ string       : String,
                            separator      : Character = "&",
@@ -27,20 +30,297 @@ public enum qs {
                            depth          : Int       = 5,
                            parameterLimit : Int       = 1000,
                            arrayLimit     : Int       = 20,
-                           allowsDot      : Bool      = false)
+                           allowsDot      : Bool      = false,
+                           decodeURIComponent :
+                                       (( String ) -> String) = _unescape)
                      -> Dictionary<String, Any>
   {
     if allowsDot { fatalError("allowsDot unsupported") }
+
+    let parsedQV = RefStringAnyDictionary()
+    var count = 0
+    let pairs = string.characters.split(separator: separator,
+                                        omittingEmptySubsequences: true)
+    for pair in pairs {
+      guard count < parameterLimit else { break }
+      count += 1
+      
+      let pairParts = pair.split(separator: pairSeparator,
+                                 maxSplits: 1,
+                                 omittingEmptySubsequences: true)
+      guard !pairParts.isEmpty else { continue }
     
-    let qv = querystring.parse(string,
-                               separator: separator,
-                               pairSeparator: pairSeparator)
+      print("PAIR: \(String(pair))")
+      let key   = decodeURIComponent(String(pairParts[0]))
+      let value = pairParts.count > 1
+                  ? decodeURIComponent(String(pairParts[1])) : ""
+          
+      let kp = parseKeyPath(key, depth: depth, allowsDot: allowsDot)
+      guard kp.count > 1 else {
+        parsedQV[key] = value
+        continue
+      }
+      
+      // TODO: validate kp, e.g. must begin with name
+      
+      // Oh my. All this code :-0 This needs a rewrite, ugly.
+      // FIXME: I don't like this :-)
+      
+      enum CursorType {
+        case Dict(RefStringAnyDictionary)
+        case Array(RefAnyArray)
+        case Other(Any)
+      }
+      
+      var cursor = CursorType.Dict(parsedQV)
+      
+      for idx in 0..<(kp.count - 1) {
+        let part = kp[idx]
+        let next = kp[idx + 1]
+        
+        switch part {
+          case .Key(let name):
+            guard case .Dict(let cursorDict) = cursor else {
+              assertionFailure("expected dictionary for key")
+              break
+            }
+            
+            if let currentValue = cursorDict[name] { // has a value already
+              if case .Key(_) = next {
+                // if the next is a key, we must have a dictionary
+                if currentValue is RefStringAnyDictionary {
+                  // all good
+                }
+                else if let array = currentValue as? RefAnyArray {
+                  let newValue = RefStringAnyDictionary()
+                  for i in 0..<array.count {
+                    newValue["\(i)"] = array[i]
+                  }
+                  cursorDict[name] = newValue
+                }
+                else {
+                  assertionFailure("unexpected object: \(currentValue)")
+                  break
+                }
+              }
+            }
+            else { // no value
+              switch next {
+                case .Key(_):
+                  cursorDict[name] = RefStringAnyDictionary()
+                case .Array, .Index(_):
+                  cursorDict[name] = RefAnyArray()
+                case .Error(let msg):
+                  assertionFailure("error: \(msg)")
+                  break
+              }
+            }
+          
+            if let nextCursor = cursorDict[name] {
+              if let rd = nextCursor as? RefStringAnyDictionary {
+                cursor = .Dict(rd)
+              }
+              else if let ra = nextCursor as? RefAnyArray {
+                cursor = .Array(ra)
+              }
+              else {
+                assertionFailure("unexpected object: \(nextCursor)")
+                cursor = .Other(nextCursor)
+              }
+            }
+          
+          case .Array:
+            // e.g.: "a[][b]=10&a[][c]=20"
+            guard case .Array(let cursorArray) = cursor else {
+              assertionFailure("expected array for key")
+              break
+            }
+            // w/ [] we always append aka create a new value
+            switch next {
+              case .Key(_):
+                let nextValue = RefStringAnyDictionary()
+                cursorArray.append(nextValue)
+                cursor = .Dict(nextValue)
+              case .Array, .Index(_):
+                let nextValue = RefAnyArray()
+                cursorArray.append(nextValue)
+                cursor = .Array(nextValue)
+              case .Error(let msg):
+                assertionFailure("error: \(msg)")
+                break
+            }
+          
+          case .Index(let idx):
+            switch cursor {
+              case .Dict(let cursorDict): // mixed
+                let name = "\(idx)"
+                if let _ /*currentValue*/ = cursorDict[name] {
+                  // TODO
+                }
+                else {
+                  // TODO
+                }
+                assert(false, "mixed values, unsupported")
+                break
+              
+              case .Array(let cursorArray):
+                cursorArray.ensureSize(idx + 1,
+                                       defaultValue: sparseArrayDefaultValue)
+                let currentValue = cursorArray[idx]
+                if !(currentValue is EmptyArraySlot) {
+                  if let rd = currentValue as? RefStringAnyDictionary {
+                    cursor = .Dict(rd)
+                  }
+                  else if let ra = currentValue as? RefAnyArray {
+                    cursor = .Array(ra)
+                  }
+                  else {
+                    assertionFailure("unexpected object: \(currentValue)")
+                    cursor = .Other(currentValue)
+                  }
+                }
+                else {
+                  switch next {
+                    case .Key(_):
+                      let nextValue = RefStringAnyDictionary()
+                      cursorArray[idx] = nextValue
+                      cursor = .Dict(nextValue)
+                    case .Array, .Index(_):
+                      let nextValue = RefAnyArray()
+                      cursorArray[idx] = nextValue
+                      cursor = .Array(nextValue)
+                    case .Error(let msg):
+                      assertionFailure("error: \(msg)")
+                      break
+                  }
+                }
+              
+              default:
+                assertionFailure("unexpected object: \(cursor)")
+            }
+          
+          default:
+            assertionFailure("not implemented: non-dict part: \(part)")
+            break
+        }
+      }
+      
+      let lastPart = kp[kp.count - 1]
+      switch lastPart {
+        case .Key(let name):
+          guard case .Dict(let cursorDict) = cursor else {
+            assertionFailure("expected dictionary for key")
+            break
+          }
+          cursorDict[name] = value
+        
+        case .Array:
+          guard case .Array(let cursorArray) = cursor else {
+            assertionFailure("expected array")
+            break
+          }
+          cursorArray.append(value)
+        
+        case .Index(let idx):
+          switch cursor {
+            case .Dict(let cursorDict):
+              cursorDict["\(idx)"] = value
+            case .Array(let cursorArray):
+              cursorArray.ensureSize(idx + 1,
+                                     defaultValue: sparseArrayDefaultValue)
+              cursorArray[idx] = value
+            default:
+              assertionFailure("unexpected object: \(cursor)")
+          }
+        
+        default:
+          assertionFailure("not implemented: non-dict part: \(lastPart)")
+          break
+      }      
+    }
     
-    // TODO: do the 'abc[def]' decoding thing
-    
-    return qv
+    return parsedQV.flatten() as? Dictionary<String, Any> ?? [:]
   }
 }
+
+
+// Hm
+
+protocol RefTypeFlatten: class {
+  
+  func flatten() -> Any
+  
+}
+
+typealias RefAnyArray            = RefArray<Any>
+typealias RefStringAnyDictionary = RefDictionary<String, Any>
+
+class RefArray<Element>: RefTypeFlatten {
+  
+  var storage = Array<Element>()
+  
+  var isEmpty : Bool { return storage.isEmpty }
+  var count   : Int  { return storage.count   }
+  
+  subscript(key: Int) -> Element {
+    set { storage[key] = newValue }
+    get { return storage[key]     }
+  }
+
+  func ensureSize(_ size: Int, defaultValue: @autoclosure () -> Element) {
+    guard count < size else { return }
+    
+    let value = defaultValue()
+    for _ in count..<size {
+      append(value)
+    }
+  }
+  
+  func append(_ element: Element) {
+    storage.append(element)
+  }
+
+  func flatten() -> Any {
+    var fstorage = Array<Any>()
+    fstorage.reserveCapacity(count)
+    
+    for value in storage {
+      if let rd = value as? RefTypeFlatten {
+        fstorage.append(rd.flatten())
+      }
+      else {
+        fstorage.append(value)
+      }
+    }
+    return fstorage
+  }
+}
+
+class RefDictionary<Key : Hashable, Value>: RefTypeFlatten {
+  
+  var storage = Dictionary<Key, Value>()
+  
+  var isEmpty : Bool { return storage.isEmpty }
+  
+  subscript(key: Key) -> Value? {
+    set { storage[key] = newValue }
+    get { return storage[key]     }
+  }
+  
+  func flatten() -> Any {
+    var fstorage = Dictionary<Key, Any>()
+    for ( key, value ) in storage {
+      if let rd = value as? RefTypeFlatten {
+        fstorage[key] = rd.flatten()
+      }
+      else {
+        fstorage[key] = value
+      }
+    }
+    return fstorage
+  }
+}
+
 
 // 'foo[bar][baz]=foobarbaz' -> [ "foo": [ "bar": [ "baz": "foobarbaz" ] ] ]
 // 'a[]=b&a[]=c')            -> [ "a": [ "b", "c" ] ]
@@ -179,4 +459,12 @@ extension qs {
     return parts
   }
   
+}
+
+
+import Foundation
+
+/// %-unescape a string.
+private func _unescape(string: String) -> String {
+  return string.removingPercentEncoding ?? string
 }
--- Tests/ExExpressTests/QueryStringTests.swift
+++ Tests/ExExpressTests/QueryStringTests.swift
@@ -11,6 +11,23 @@ import XCTest
 
 class QueryStringTests: XCTestCase {
  
+  let sample1struct : [ String : Any ] = [
+    "length": "10",
+    "search": ["value": "", "regex": "false"],
+    "_": "1493677490768",
+    "draw": "1",
+    "start": "0",
+    "order": [["dir": "asc", "column": "0"]],
+    "columns": [
+      [ "orderable": "true", "name": "", "data": "login",
+        "search": ["value": "", "regex": "false"], "searchable": "true"],
+      [ "orderable": "true", "name": "", "data": "name",
+        "search": ["value": "", "regex": "false"], "searchable": "true"],
+      [ "orderable": "true", "name": "", "data": "cities",
+        "search": ["value": "", "regex": "false"], "searchable": "true"]
+    ]
+  ]
+  
   let sample1 = "draw=1&columns%5B0%5D%5Bdata%5D=login&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=name&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=cities&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1493677490768"
 
   let queryKeyNested = "foo[bar][baz]"
@@ -35,6 +52,116 @@ class QueryStringTests: XCTestCase {
     XCTAssertNotNil(qp["search[value]"])
     XCTAssertEqual(qp["length"] as? String, "10")
   }
+  
+  
+  // MARK: - QS tests
+  
+  func testNestedQS() throws {
+    let parameters = qs.parse("foo[bar][baz]=foobarbaz")
+    XCTAssertEqual(parameters.count, 1)
+    
+    let o1 = parameters["foo"]
+    XCTAssertNotNil(o1)
+    XCTAssert(o1 is Dictionary<String, Any>)
+    
+    let o2 = (o1 as! Dictionary<String, Any>)["bar"]
+    XCTAssertNotNil(o2)
+    XCTAssert(o2 is Dictionary<String, Any>)
+
+    let o3 = (o2 as! Dictionary<String, Any>)["baz"]
+    XCTAssertNotNil(o3)
+    XCTAssert(o3 is String)
+    XCTAssertEqual(o3 as! String, "foobarbaz")
+  }
+  
+  func testSimpleArrayQS() throws {
+    let parameters = qs.parse("a[]=b&a[]=c")
+    XCTAssertEqual(parameters.count, 1)
+    
+    let o1 = parameters["a"]
+    XCTAssertNotNil(o1)
+    XCTAssert(o1 is Array<Any>)
+    
+    let o1a = o1 as! Array<Any>
+    XCTAssertEqual(o1a.count, 2)
+    XCTAssert(o1a.contains(where: { $0 as? String == "b" }))
+    XCTAssert(o1a.contains(where: { $0 as? String == "c" }))
+  }
+  
+  func testIndexedArrayQS() throws {
+    let parameters = qs.parse("a[1]=b&a[3]=c")
+    XCTAssertEqual(parameters.count, 1)
+
+    let o1 = parameters["a"]
+    XCTAssertNotNil(o1)
+    XCTAssert(o1 is Array<Any>)
+    
+    let o1a = o1 as! Array<Any>
+    XCTAssertEqual(o1a.count, 4)
+    XCTAssertEqual(o1a[1] as? String, "b")
+    XCTAssertEqual(o1a[3] as? String, "c")
+  }
+  
+  func testArrayOfObjectsQS() throws {
+    // 'a[][b]=c'                -> [ "a": [ [ "b": "c" ] ] ]
+    let parameters = qs.parse("a[][b]=c&a[][c]=42")
+    XCTAssertEqual(parameters.count, 1)
+    
+    let o1 = parameters["a"]
+    XCTAssertNotNil(o1)
+    XCTAssert(o1 is Array<Any>)
+    
+    let o1a = o1 as! Array<Any>
+    XCTAssertEqual(o1a.count, 2)
+    
+    let o2 = o1a[0]
+    XCTAssert(o2 is Dictionary<String, Any>)
+    let o2t = o2 as! Dictionary<String, Any>
+    XCTAssertEqual(o2t.count, 1)
+    XCTAssertEqual(o2t["b"] as? String, "c")
+    
+    let o3 = o1a[1]
+    XCTAssert(o3 is Dictionary<String, Any>)
+    let o3t = o3 as! Dictionary<String, Any>
+    XCTAssertEqual(o3t.count, 1)
+    XCTAssertEqual(o3t["c"] as? String, "42")
+  }
+  
+  func testDataTablesQS() throws {
+    let parameters = qs.parse(sample1)
+    XCTAssertEqual(parameters.count, 7)
+    
+    XCTAssertEqual(parameters["length"] as? String, "10")
+    XCTAssertEqual(parameters["draw"]   as? String, "1")
+    XCTAssertEqual(parameters["start"]  as? String, "0")
+    XCTAssertEqual(parameters["_"]      as? String, "1493677490768")
+
+    let columns = parameters["columns"]
+    XCTAssertNotNil(columns)
+    XCTAssert(columns is Array<Any>)
+    let tColumns = columns as! Array<Any>
+    XCTAssertEqual(tColumns.count, 3)
+    
+    let col0 = tColumns[0]
+    XCTAssert(col0 is Dictionary<String, Any>)
+    let col0t = col0 as! Dictionary<String, Any>
+    XCTAssertEqual(col0t.count, 5)
+    
+    XCTAssertEqual(col0t["orderable"]  as? String, "true")
+    XCTAssertEqual(col0t["searchable"] as? String, "true")
+    XCTAssertEqual(col0t["name"]       as? String, "")
+    XCTAssertEqual(col0t["data"]       as? String, "login")
+    
+    let col0search = col0t["search"]
+    XCTAssert(col0search is Dictionary<String, Any>)
+    let col0searchT = col0search as! Dictionary<String, Any>
+    XCTAssertEqual(col0searchT.count, 2)
+    XCTAssertEqual(col0searchT["value"] as? String, "")
+    XCTAssertEqual(col0searchT["regex"] as? String, "false")
+  }
+  
+  
+  // MARK: - KeyPath parser tests
   
   func testNestedKey() throws {
     let keyPath = qs.parseKeyPath(queryKeyNested, allowsDot: false)
@@ -120,5 +247,6 @@ class QueryStringTests: XCTestCase {
     if case .Key(let value) = keyPath[2] { XCTAssertEqual(value, "[c][d]") }
     else { XCTAssert(false, "not a string key") }
   }
+  
 }
 
