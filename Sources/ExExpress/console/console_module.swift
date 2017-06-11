1a39de8cd8c95fa9c6c64ede34ee9cf7ce7ae8fc
Helge Hess <devteam@zeezide.de>
2017-06-11 15:09:56 +0200

    Make defaultConsole public
    ... can be useful in other places.

--- Sources/ExExpress/console/console_module.swift
+++ Sources/ExExpress/console/console_module.swift
@@ -15,9 +15,9 @@ public enum console {
   // FIXME: Obviously this should be hooked up to Apache logging. For this to
   //        work we would probably need thread-local storage to be able to
   //        direct the logs to a proper request logger.
-  static var defaultConsole : ConsoleType =
-               Console2(FileOutputStream(handle: stdout),
-                        FileOutputStream(handle: stderr))
+  public static var defaultConsole : ConsoleType =
+                      Console2(FileOutputStream(handle: stdout),
+                               FileOutputStream(handle: stderr))
   
   // top level functions, so that you can do: console.info() ...
   // Note: yes, we could name `defaultConsole` 'console', but then stuff
