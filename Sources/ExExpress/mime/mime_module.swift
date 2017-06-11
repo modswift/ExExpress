43deb60766f176c51cd18df8c3cf4fa02ef5ae34
Helge Hess <devteam@zeezide.de>
2017-06-11 16:22:38 +0200

    Add a very simple 'mime' module
    ... this needs a lot more types :-)

---
+++ Sources/ExExpress/mime/mime_module.swift
@@ -0,0 +1,66 @@
+//
+//  mime_module.swift
+//  ExExpress
+//
+//  Created by Helge Hess on 11/06/17.
+//  Copyright © 2017 ZeeZide GmbH. All rights reserved.
+//
+
+// desired API:
+//   https://www.npmjs.com/package/mime-types
+
+import struct Foundation.URL
+
+public enum mime {
+  
+  public static func lookup(_ path: String) -> String? {
+    if !path.contains(".") && !path.contains("/") {
+      if let ctype = types[path] {
+        if let cs = defaultCharsets[ctype] {
+          return ctype + "; charset=" + cs
+        }
+        return ctype
+      }
+    }
+    
+    let url = Foundation.URL(fileURLWithPath: path)
+    return types[url.pathExtension]
+  }
+  
+  public static func charset(_ ctype: String) -> String? {
+    return defaultCharsets[ctype]
+  }
+
+  public static let types : [ String : String ] = [
+    "html":     "text/html",
+    "js":       "application/javascript",
+    "ico":      "image/x-icon",
+    "svg":      "image/svg+xml",
+    "eot":      "application/vnd.ms-fontobject",
+    "woff":     "application/x-font-woff",
+    "woff2":    "application/x-font-woff",
+    "ttf":      "application/x-font-ttf",
+    "markdown": "text/x-markdown",
+    "json":     "application/json"
+  ]
+  
+  public static let extensions : [ String : [ String ] ] = [
+    "text/html":                     [ "html" ],
+    "application/javascript":        [ "js"   ],
+    "image/x-icon":                  [ "ico"  ],
+    "image/svg+xml":                 [ "svg"  ],
+    "application/vnd.ms-fontobject": [ "eot"  ],
+    "application/x-font-woff":       [ "woff", "woff2" ],
+    "application/x-font-ttf":        [ "ttf"  ],
+    "text/x-markdown":               [ "markdown" ],
+    "application/json":              [ "json" ]
+  ]
+
+  static let defaultCharsets : [ String : String ] = [
+    "text/html":              "UTF-8",
+    "application/javascript": "UTF-8",
+    "image/svg+xml":          "UTF-8",
+    "text/x-markdown":        "UTF-8",
+    "application/json":       "UTF-8"
+  ]
+}
