265ee80179711d67f57c4010f80fadf81f63a5f2
Helge Hess <devteam@zeezide.de>
2017-07-25 16:28:03 +0200

    Indent

--- Package.swift
+++ Package.swift
@@ -3,23 +3,23 @@ import PackageDescription
 let package = Package(
   name: "ExExpress",
 
-	dependencies: [
-	  .Package(url: "https://github.com/AlwaysRightInstitute/mustache.git", 
-		         majorVersion: 0, minor: 5),
+  dependencies: [
+    .Package(url: "https://github.com/AlwaysRightInstitute/mustache.git",
+             majorVersion: 0, minor: 5),
     
- 	  .Package(url: "https://github.com/modswift/Freddy.git", 
- 		         majorVersion: 3, minor: 0)
+    .Package(url: "https://github.com/modswift/Freddy.git",
+             majorVersion: 3, minor: 0)
     /* 3.0.2 fails on Linux
 	  .Package(url: "https://github.com/bignerdranch/Freddy.git",
 						 majorVersion: 3, minor: 0),
      */
   ],
 	
-	exclude: [
-		"ExExpress.xcodeproj",
-		"GNUmakefile",
-		"LICENSE",
-		"README.md",
-		"xcconfig"
-	]
+  exclude: [
+    "ExExpress.xcodeproj",
+    "GNUmakefile",
+    "LICENSE",
+    "README.md",
+    "xcconfig"
+  ]
 )
