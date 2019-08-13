1a49d765793a7dccbef6d0d1c75795780d072021
Helge Heß <devteam@zeezide.de>
2019-08-13 15:14:27 +0200

    Add Swift 5 manifest
    ...

---
+++ Package@swift-5.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.0
+//
+//  Package.swift
+//  ExExpress
+//
+//  Created by Helge Hess on 11.05.18.
+//  Copyright © 2019 ZeeZide. All rights reserved.
+//
+import PackageDescription
+
+let package = Package(
+  name: "ExExpress",
+
+  products: [
+    .library(name: "ExExpress", targets: [ "ExExpress" ]),
+  ],
+  
+  dependencies: [
+    .package(url: "https://github.com/AlwaysRightInstitute/mustache.git",
+             from: "0.5.9"),
+    .package(url: "https://github.com/modswift/Freddy.git",
+             from: "3.0.58")
+  ],
+	
+  targets: [
+    .target(name: "ExExpress", dependencies: [ "mustache", "Freddy" ])
+  ]
+)
